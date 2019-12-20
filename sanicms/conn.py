242eaa2b42ea9b936295f65ebbb0ab585f8bade4
Evan Kao <jiantao.gao@cityio.cn>
2019-12-20 10:50:45 +0800

    update

--- sanicms/conn.py
+++ sanicms/conn.py
@@ -18,7 +18,7 @@ redis_conn = redis.Redis(connection_pool=redis_pool)
 
 
 # 创建rabbitmq连接
-rabbitmq_conn = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+rabbitmq_conn = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT))
 
 """
 show variables like 'max_connections'; 查看数据库最大连接数
