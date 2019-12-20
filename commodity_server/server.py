c892279a19d462d3919f61635c7e876ed3ba064e
Evan Kao <jiantao.gao@cityio.cn>
2019-12-20 15:35:30 +0800

    update

--- commodity_server/server.py
+++ commodity_server/server.py
@@ -17,13 +17,13 @@ app.blueprint(commodity_bp)
 async def before_srver_start(app, loop):
     # TODO: 改为配置文件读取相关依赖应用服务 或则获取统一的服务注册中心 获取相关服务
     app.user_client = Client('user-service', app=app)
-    app.order_client = Client('order-service', app=app)
+    # app.order_client = Client('order-service', app=app)
 
 
 @app.listener('before_server_stop')
 async def before_server_stop(app, loop):
     app.user_client.close()
-    app.order_client.close()
+    # app.order_client.close()
 
 
 @app.route("/")
